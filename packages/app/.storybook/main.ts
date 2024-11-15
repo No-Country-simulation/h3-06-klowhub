bd056ad0fef23699bbc2849af12695b94d470ee3
Maria Villen <9887543+MariaVillen@users.noreply.github.com>
2024-11-15 11:36:15 +0100

    storybook configuration

---
+++ packages/app/.storybook/main.ts
@@ -0,0 +1,28 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+
+import path from 'path';
+
+const getAbsolutePath = (packageName: string): any =>
+  path.dirname(require.resolve(path.join(packageName, 'package.json')));
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    getAbsolutePath('@storybook/addon-onboarding'),
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@chromatic-com/storybook'),
+    getAbsolutePath('@storybook/addon-interactions'),
+    getAbsolutePath('@storybook/theming'),
+    getAbsolutePath('@storybook/manager-api'),
+    getAbsolutePath('@storybook/addon-styling-webpack'),
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+    defaultName: 'Documentation',
+  },
+};
+export default config;
