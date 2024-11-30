86af1e61e70f43554a3f520359c5203312277b7b
damianfe <damianrocio12@gmail.com>
2024-11-29 21:33:19 -0300

    refactor(swagger): move configuration to a separate file for better structure

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { setupSwagger } from './infrastructure/config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,10 +12,8 @@ async function bootstrap() {
   const port = configService.get<number>('PORT') || 3000;
 
   app.enableCors();
-
   app.use(helmet());
 
-  // Configurar ValidationPipe global
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -25,16 +23,7 @@ async function bootstrap() {
     }),
   );
 
-  // Configurar Swagger
-  const swaggerConfig = new DocumentBuilder()
-    .setTitle('API de KlowHub')
-    .setDescription('Documentación de la API de KlowHub')
-    .setVersion('1.0')
-    .addBearerAuth() // Habilitar autenticación con JWT (opcional)
-    .build();
-
-  const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api/docs', app, document);
+  setupSwagger(app);
 
   await app.listen(port);
   console.log(`Application is running on: http://localhost:${port}`);
